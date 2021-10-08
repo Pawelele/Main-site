54726c419f82d317f90c01ff872e9b48b6b27b65
Paweł Uchański <uchanski.pawel@gmail.com>
2021-10-08 21:27:46 +0200

    Add projects popups with descriptions

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="author" content="Paweł Uchański">
 		<meta name="description" content="Front-End developer Paweł Uchański">
-		<meta name="keywords" content="Paweł Uchański, Front-End, Paweł, Uchański, Programista, Programista Uchański">
+		<meta name="keywords" content="Paweł Uchański, Front-End, Paweł, Uchański, Programista, Programista Uchański, Programista Paweł Uchański, Pawel Uchanski">
 		<link rel="stylesheet" href="style.css" type="text/css">
 
         <!-- Czcionka -->
@@ -19,19 +19,17 @@
         <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
         <!-- Particles end -->
         <script src="js/popup.js" defer></script>
-
         <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-TZZQG2TEMS"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-209484079-1"></script>
         <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
-        gtag('config', 'G-TZZQG2TEMS');
+        gtag('config', 'UA-209484079-1');
         </script>
-
+        <!-- gtag end -->
     </head>
-
     <body>
         <!-- particles.js container -->
         <div id="particles-js">
@@ -57,25 +55,70 @@
                 <input type="submit" name="send_message" value="Wyślij" id="send_submit"><br>
             </form>
         </div>
-
-        <div class="project3-popup">
-            <div class="project3_left">
-                <div class="project3_logo">
-                    <img src="/img/kursly_logo.png">
+        <!-- Project 1 popup -->
+        <div class="project-popup" id="project1-popup">
+            <span class="project_x" id="project1_x">X</span>
+                <div class="project_left">
+                    <a href="https://portfolio.paweluchanski.pl" target="_blank">
+                        <div class="project_logo">
+                            <img src="/img/logo_portfolio.png">
+                        </div>
+                    </a>
+                    <p><span>Nazwa:</span> Portfolio</p>
+                    <p><span>Autor:</span> Paweł Uchański</p>
+                    <p><span>Status:</span> Oczekuje na poprawki</p>
+                    <p><span>Github:</span> <a href="https://github.com/Pawelele/Projekt-AiPWI" target="_blank">LINK</a></p>
                 </div>
-                <p><span>Nazwa:</span> Kursly</p>
-                <p><span>Autor:</span> Paweł Uchański</p>
-                <p><span>Status:</span> W rozbudowie</p>
-                <p><span>Github:</span> <a href="#">LINK</a></p>
-            </div>
-            <div class="project3_right">
-                <div class="project3_txt">
-                    <h2>OPIS</h2>
-                    <p>Platforma umożliwiająca kupowanie kursów internetowych. Aplikacja powstała jako projekt na studiach. Podstawowa funkcjonalność obejmuje: logowanie, rejestrację, doładowanie wirtualnego portfela oraz kupowanie wybranych kursów.<br><br><br>Do stworzenia projektu zostały wykorzystane technologie: HTML, CSS, Bootstrap, JavaScript, PHP oraz Bazy Danych.<br><br><br> Przykładowe dane do logowania:<br> Login: test@test.pl<br> Hasło: Test_123</p>
+                <div class="project_right">
+                    <div class="project_txt">
+                        <h2>OPIS</h2>
+                        <p>Strona internetowa powstała jako projekt na studiach. Nie posiada żadnych dodatkowych funkcjonalności. Formularz na dole strony jeszcze nie działa. Finalnie za jakiś czas będzie to moje portfolio ze wszystkimi aktualnymi informacjami oraz możliwością kontaktu.<br><br>Do stworzenia projektu zostały wykorzystane technologie: HTML, CSS, Bootstrap oraz JavaScript.
+                    </div>
                 </div>
-
-            </div>
         </div>
+        <!-- Project 2 popup -->
+        <div class="project-popup" id="project2-popup">
+            <span class="project_x" id="project2_x">X</span>
+                <div class="project_left">
+                    <a href="https://urzad.paweluchanski.pl" target="_blank">
+                        <div class="project_logo">
+                            <img src="/img/logo_urzad.png">
+                        </div>
+                    </a>
+                    <p><span>Nazwa:</span> Urząd Miasta</p>
+                    <p><span>Autor:</span> Paweł Uchański</p>
+                    <p><span>Status:</span> Oczekuje na poprawki</p>
+                    <p><span>Github:</span> <a href="https://github.com/Pawelele/Projekt-SIZ" target="_blank">LINK</a></p>
+                </div>
+                <div class="project_right">
+                    <div class="project_txt">
+                        <h2>OPIS</h2>
+                        <p>Platforma umożliwiająca zapisy do urzędu miasta na konkretny dzień bez konieczności wychodzenia z domu. Dodatkowa funkcjonalność obejmuje wysłanie potwierdzenia zapisu na maila oraz sprawdzenie swoich aktywnych zapisów poprzez wpisanie numeru PESEL.<br><br>Do stworzenia projektu zostały wykorzystane technologie: HTML, CSS, Bootstrap, JavaScript, PHP oraz Bazy Danych.
+                    </div>
+                </div>
+        </div>
+        <!-- Project 3 popup -->
+        <div class="project-popup" id="project3-popup">
+            <span class="project_x" id="project3_x">X</span>
+                <div class="project_left">
+                    <a href="https://kursly.paweluchanski.pl" target="_blank">
+                        <div class="project_logo">
+                            <img src="/img/logo_kursly.png">
+                        </div>
+                    </a>
+                    <p><span>Nazwa:</span> Kursly</p>
+                    <p><span>Autor:</span> Paweł Uchański</p>
+                    <p><span>Status:</span> W rozbudowie</p>
+                    <p><span>Github:</span> <a href="https://github.com/Pawelele/Projekt-IAI" target="_blank">LINK</a></p>
+                </div>
+                <div class="project_right">
+                    <div class="project_txt">
+                        <h2>OPIS</h2>
+                        <p>Platforma umożliwiająca kupowanie kursów internetowych. Aplikacja powstała jako projekt na studiach. Podstawowa funkcjonalność obejmuje: logowanie, rejestrację, doładowanie wirtualnego portfela oraz kupowanie wybranych kursów.<br><br>Do stworzenia projektu zostały wykorzystane technologie: HTML, CSS, Bootstrap, JavaScript, PHP oraz Bazy Danych.<br><br> Przykładowe dane do logowania:<br> Login: test@test.pl<br> Hasło: Test_123</p>
+                    </div>
+                </div>
+        </div>
+        <!-- Project popup end -->
 
         <div class="menu">
             <a href="https://linkedin.com/in/pawel-uchanski" target="_blank"><img src="img/linkedin.png"></a>
@@ -87,9 +130,9 @@
             <a href="/"><img class="logo" src="img/logo.png"></a>
             <p class="title">Paweł Uchański</p>
             <p class="subtitle">Projekty:</p>
-            <a href="https://portfolio.paweluchanski.pl" target="_blank"><div class="button">Portfolio</div></a><br>
-            <a href="https://urzad.paweluchanski.pl" target="_blank"><div class="button">Urząd Miasta</div></a><br>
-            <a href="https://kursly.paweluchanski.pl" target="_blank"><div class="button">Kursly</div></a><br>
+            <div class="button" id="project1">Portfolio</div><br>
+            <div class="button" id="project2">Urząd Miasta</div><br>
+            <div class="button" id="project3">Kursly</div><br>
 
         </div>
 
--- js/popup.js
+++ js/popup.js
@@ -1,23 +1,101 @@
-let icon = document.querySelector("#send_message");
-let popup = document.querySelector(".contact-popup");
-let exit = document.querySelector("#contact_x");
+// Contact popup
+let icon_contact = document.querySelector("#send_message");
+let popup_contact = document.querySelector(".contact-popup");
+let exit_contact = document.querySelector("#contact_x");
 
-let opened = false;
+let opened_contact = false;
 
-icon.addEventListener("click",function(){
-    if(opened == false)
+icon_contact.addEventListener("click",function(){
+    if(opened_contact == false)
     {
-        popup.style.display = "block";
-        opened = true;
+        popup_contact.style.display = "block";
+        opened_contact = true;
     }
     else
     {
-        popup.style.display = "none";
-        opened = false;
+        popup_contact.style.display = "none";
+        opened_contact = false;
     }
 });
 
-exit.addEventListener("click", function(){
-    popup.style.display = "none";
-    opened = false;
+exit_contact.addEventListener("click", function(){
+    popup_contact.style.display = "none";
+    opened_contact = false;
 });
+
+
+//Project1 popup
+let button_project1 = document.querySelector("#project1");
+let popup_project1 = document.querySelector("#project1-popup");
+let exit_project1 = document.querySelector("#project1_x");
+
+let opened_project1 = false;
+
+button_project1.addEventListener("click",function(){
+    if(opened_project1 == false)
+    {
+        popup_project1.style.display="block";
+        opened_project1 = true;
+    }
+    else
+    {
+        popup_project1.style.display="none";
+        opened_project1 = false;
+    }
+});
+
+exit_project1.addEventListener("click", function(){
+    popup_project1.style.display = "none";
+    opened_project1 = false;
+});
+
+//Project2 popup
+let button_project2 = document.querySelector("#project2");
+let popup_project2 = document.querySelector("#project2-popup");
+let exit_project2 = document.querySelector("#project2_x");
+
+let opened_project2 = false;
+
+button_project2.addEventListener("click",function(){
+    if(opened_project2 == false)
+    {
+        popup_project2.style.display="block";
+        opened_project2 = true;
+    }
+    else
+    {
+        popup_project2.style.display="none";
+        opened_project2 = false;
+    }
+});
+
+exit_project2.addEventListener("click", function(){
+    popup_project2.style.display = "none";
+    opened_project2 = false;
+});
+
+// Project3 popup
+let button_project3 = document.querySelector("#project3");
+let popup_project3 = document.querySelector("#project3-popup");
+let exit_project3 = document.querySelector("#project3_x");
+
+let opened_project3 = false;
+
+button_project3.addEventListener("click",function(){
+    if(opened_project3 == false)
+    {
+        popup_project3.style.display="block";
+        opened_project3 = true;
+    }
+    else
+    {
+        popup_project3.style.display="none";
+        opened_project3 = false;
+    }
+});
+
+exit_project3.addEventListener("click", function(){
+    popup_project3.style.display = "none";
+    opened_project3 = false;
+});
+
